213df3b6e2f10ae934bdcf06d04c8072e8a4bdc6
E2057SalihPoyraz <65684545+E2057SalihPoyraz@users.noreply.github.com>
2020-07-07 02:21:13 +0300

    Create Assignment-1_if_statements.py

---
+++ Assignment-1_if_statements.py
@@ -0,0 +1,7 @@
+my_name = "Salih"
+name = input("Lütfen isminizi giriniz: ")
+
+if name == my_name:
+    print("Hello", name, "\b! The password is : Well_Done")
+else:
+    print("Hello", name, "\b! See you later")
